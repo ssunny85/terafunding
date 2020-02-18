7a9782708784dd5437060343bb2516e767300d14
ssunny <ssssunny85@gmail.com>
2020-02-18 21:39:15 +0900

    Add common.scss

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  metaInfo: {
+    title: '테라펀딩',
+    titleTemplate: (titleChunk) => (titleChunk ? `${titleChunk}` : '테라펀딩'),
+    htmlAttrs: {
+      lang: 'ko',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { vmid: 'keywords', name: 'keywords', content: '테라펀딩, 테라핀테크, P2P투자, P2P대출, 부동산투자, P2P업계1위' },
+      { vmid: 'description', name: 'description', content: '건축주와 투자자를 연결하는 새로운 금융 플랫폼 테라펀딩은 자금이 필요한 중소형 건축주들과 새로운 투자처가 필요한 소액 투자자들을 연결해주는 P2P 금융 플랫폼입니다.' },
+    ],
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style src="./assets/scss/common.scss" lang="scss" />
---
+++ src/views/product/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="product-wrap">
+    <div class="product-filter">
+      <div class="product-filter__inner">
+        <div class="product-filter__item">
+          <h2 class="product-filter__label">상품유형</h2>
+          <span class="">
+            <label for="contract-all">전체</label>
+            <input type="checkbox" id="contract-all" />
+          </span>
+          <span class="">
+            <label for="contract-funds">건축자금</label>
+            <input type="checkbox" id="contract-funds" />
+          </span>
+          <span class="">
+            <label for="contract-guarantee">부동산담보</label>
+            <input type="checkbox" id="contract-guarantee" />
+          </span>
+        </div>
+        <div class="">
+          <h2 class="">채권상태</h2>
+          <span class="">
+            <label for="status-all">전체</label>
+            <input type="checkbox" id="status-all" />
+          </span>
+          <span class="">
+            <label for="status-waiting">대기중</label>
+            <input type="checkbox" id="status-waiting" />
+          </span>
+          <span class="">
+            <label for="status-recruiting">모집중</label>
+            <input type="checkbox" id="status-recruiting" />
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="product-list">
+      <p></p>
+      <div class="">
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductList',
+  components: {},
+  metaInfo: {
+    title: '상품 목록',
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style src="./style.scss" lang="scss" scoped />
